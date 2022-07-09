07d9b9553334e0480bba1a7b3034910d9d2cd11d
Ariffadillahh <fadilaharif99@gmail.com>
2022-07-09 18:55:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import Logo from "./source/logo.png";
+import { GrFormClose } from "react-icons/gr";
+import { GoThreeBars } from "react-icons/go";
+import Hero from "./components/Hero";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Galery from "./components/Galery";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import News1 from "./components/News1";
+
+function App() {
+  const [bg, setBg] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      return window.scrollY > 30 ? setBg(true) : setBg(false);
+    });
+  });
+
+  const [Nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!Nav);
+  };
+
+  return (
+    <Router>
+      <nav>
+        <div className="w-full h-[85px] ">
+          <div className={`${bg ? "fixed w-full h-20 shadow-lg z-[100] bg-white/95 duration-300 " : "flex w-full h-20 "} transition duration-300`}>
+            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+              <Link to="/">
+                <img src={Logo} alt="" width="100" height="100" />
+              </Link>
+              <ul className="hidden mr-[4rem]  md:flex">
+                <li>
+                  <Link to="/" spy={true} smooth={true} offset={50} duration={500} className="pl-4 uppercase hover:text-[#7AAEC5] duration-300">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/About" spy={true} smooth={true} offset={50} duration={500} className="pl-4 uppercase hover:text-[#7AAEC5] duration-300">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Galery" spy={true} smooth={true} offset={50} duration={500} className="pl-4 uppercase hover:text-[#7AAEC5] duration-300">
+                    Galery
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Contact" spy={true} smooth={true} offset={50} duration={500} className="pl-4 uppercase hover:text-[#7AAEC5] duration-300">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+              <div onClick={handleNav} className="pr-7 md:hidden cursor-pointer">
+                {!Nav ? <GrFormClose size={40} /> : <GoThreeBars size={30} />}
+              </div>
+            </div>
+            <div className={!Nav ? "fixed pt-5 top-[95px] right-9 w-[250px] h-[200px] bg-white rounded-lg shadow-2xl md:hidden ease-in-out duration-300 z-[100] " : "fixed right-[-100%] top-[95px] ease-in-out duration-300   "}>
+              <ul>
+                <li>
+                  <Link to="/" spy={true} smooth={true} offset={50} duration={500} className="text-base text-slate-800 font-bold py-2 mx-8 flex transition hover:text-[#7AAEC5] duration-300 uppercase border-b-2 ">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/About" spy={true} smooth={true} offset={50} duration={500} className="text-base text-slate-800 font-bold py-2 mx-8 flex transition hover:text-[#7AAEC5] duration-300 uppercase border-b-2">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Galery" spy={true} smooth={true} offset={50} duration={500} className="text-base text-slate-800 font-bold py-2 mx-8 flex transition hover:text-[#7AAEC5] duration-300 uppercase border-b-2">
+                    Galery
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Contact" spy={true} smooth={true} offset={50} duration={500} className="text-base text-slate-800 font-bold py-2 mx-8 flex transition hover:text-[#7AAEC5] duration-300 uppercase">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Galery" element={<Galery />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/News1" element={<News1 />} />
+      </Routes>
+      <footer>
+        <div className="p-10 bg-gray-800 text-gray-200 ">
+          <div className="max-w-7xl mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2  xl:pr-10">
+              <div className="mb-5">
+                <h4 className="text-xl pb-4 lg:pl-[40px]">Rekayasa Perangkat Lunak</h4>
+                <p className="text-gray-500 lg:pl-[40px]">
+                  JL. Pegantin Ali Ciracas Jakarta Timur
+                  <strong className="block">Phone : 083804450112</strong>
+                  <strong>Email : rpl.bhj@gmail.com </strong>
+                </p>
+              </div>
+              <div className="mb-5 lg:pl-[100px]">
+                <h4 className="text-xl">Useful Links</h4>
+                <ul className="text-gray-500">
+                  <li className="pb-3 pt-2 hover:text-slate-300 transition duration-300 ">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li className="pb-3 hover:text-slate-300 transition duration-300">
+                    <Link to="/About">About</Link>
+                  </li>
+                  <li className="pb-3 hover:text-slate-300 transition duration-300">
+                    <Link to="/Galery">Gallery</Link>
+                  </li>
+                  <li className="pb-3 hover:text-slate-300 transition duration-300">
+                    <Link to="/Contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="mb-5">
+                <h4 className="text-xl">Sosial Media</h4>
+                <ul className="text-gray-500">
+                  <li className="pb-3 pt-2">
+                    <a href="https://www.instagram.com/rplbhj/" target="_blank" className="pb-3 pt-2 hover:text-slate-300 transition duration-300 ">
+                      Instagram
+                    </a>
+                  </li>
+                  <li className="pb-3">
+                    <a href="https://www.youtube.com/channel/UChGLYoQoqJ9ecsjQeB9oMeA" target="_blank" className="pb-3 pt-2 hover:text-slate-300 transition duration-300 ">
+                      Youtube
+                    </a>
+                  </li>
+                  <li className="pb-3">
+                    <a href="https://www.facebook.com/akademifarmasi.husada" target="_blank" className="pb-3 pt-2 hover:text-slate-300 transition duration-300 ">
+                      Facebook
+                    </a>
+                  </li>
+                  <li className="pb-3">
+                    <a href="https://www.tiktok.com/@fdlharip_" target="_blank" className="pb-3 pt-2 hover:text-slate-300 transition duration-300 ">
+                      TikTok
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="mb-5 ">
+                <h4 className="capitalize pb-3">Come and join us !</h4>
+                <p className="tetx-gray-500 pb-6 ">Join dan belajar bersama kami di SMK BHUMI HUSADA</p>
+                <form className="flex flex-row flex-wrap">
+                  <input type="text" name="name" id=" " className="text-gray-500 w-2/3 p-2 lg:w-full xl:w-2/3 focus:border-[#7AAEC5] rounded-l-md md:rounded-r-md lg:mt-2 xl:mt-0 lg:mr-2 xl:mr-0 " placeholder="Enter your email" />
+                  <button className="p-2 W-1/3 bg-[#7AAEC5] text-white hover:bg-[#a7cada] capitalize md:ml-2 lg:ml-0 lg:mt-2 xl:mt-0 xl:ml-2  rounded-r-md md:rounded-l-md ">subscribe</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full bg-gray-900 text-gray-500 pt-10 pb-10">
+          <div>
+            <div className="text-center">
+              <div className="uppercase">
+                ©
+                <strong>
+                  <span>RPLBHJ</span>
+                </strong>
+                . Di Buat oleh Tim RPl
+              </div>
+              <div>
+                Designed by{" "}
+                <a href="#" class="text-[#7AAEC5]">
+                  California
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,120 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import TypeWriterEffect from "react-typewriter-effect";
+import { Link } from "react-router-dom";
+import gbk from "../source/gbk.png";
+import gbk2 from "../source/gbk2.svg";
+import gbk3 from "../source/gbk3.svg";
+import bljr from "../source/foto.svg";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./style.css";
+
+// import required modules
+import { Pagination, Autoplay } from "swiper";
+
+const Hero = () => {
+  return (
+    <div id="home">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={0}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="w-full h-[220px] md:h-[330px] lg:h-[500px]  bg-cover bg-no-repeat" style={{ backgroundImage: `url(${gbk})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}>
+            <div className="content-center pt-[13%] text-white">
+              <h2 className="text-xl font-bold font-sans text-[#bbe5f8] lg:text-3xl">SMK BHUMI HUSADA</h2>
+              <h1 className="text-3xl uppercase font-bold tracking-widest font-sans lg:text-5xl">software engineer</h1>
+              <h5 className="text-base lg:text-xl lg:pt-1">"Little Things Means A Lot"</h5>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div style={{ backgroundImage: `url(${gbk2})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className="w-full h-[220px] md:h-[330px] lg:h-[500px] bg-[#FFF3CE] justify-center"></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-full h-[220px] md:h-[330px] lg:h-[500px] bg-[#EDFFD8] bg-cover bg-no-repeat content-center pt-[13%] text-white"
+            style={{ backgroundImage: `url(${gbk3})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}
+          ></div>
+        </SwiperSlide>
+      </Swiper>
+      <div>
+        <h1 className="text-center font-bold text-2xl uppercase mt-10 md:text-3xl bg-clip-text bg-gradient-to-r from-[#034867] to-[#bbe5f8] text-transparent">
+          We are software engineer<span className="block">Smk Bhumi husada</span>
+          <span className="block w-[100px] h-1 my-4 rounded-lg bg-[#034867] text-center mx-auto"></span>
+        </h1>
+        <div className="text-center">
+          <h1 className="mx-3 mt-11 md:text-lg lg:text-xl">
+            Software Engineering (RPL) adalah salah satu program keahlian yang ada di SMK Bhumi Husada yang dibuka pada tahun 2017.
+            <span className="text-black pl-1 pb-3 text-center flex  place-content-center">
+              Di SMK BHJ Belajar <span className="mx-1">Tentang</span>
+              <TypeWriterEffect startDelay={1500} cursorColor="#3F3D56" multiText={["Coding..", "Assemble pc...", "Editing Video...", "Podcast..", "And Network."]} multiTextDelay={1000} typeSpeed={30} multiTextLoop className="pl-1" />
+            </span>
+          </h1>
+        </div>
+        <div className="m-3 ">
+          <div className="md:flex ">
+            <img src={bljr} style={{ width: "600", height: "600px" }} className="flex rounded-lg drop-shadow-lg   md:w-1/2 md:h-1/2 md:mt-[5rem] lg:pt-0 md:ml-10 md:pt-8" />
+            <div className="lg:mt-[70px] md:mt-10 ">
+              <h1 className="font-bold text-xl uppercase mt-3 mdtext-base lg:w-full md:text-center bg-clip-text bg-gradient-to-r from-[#034867] to-[#bbe5f8] text-transparent">
+                our vision & mission <span className="block w-[230px] md:w-[100px] h-1 my-4 rounded-lg bg-[#034867] text-center md:mx-auto"></span>
+              </h1>
+              <h1 className="mt-3 md:ml-5 lg:text-lg md:mr-7 ">
+                Menjadi SMK yang memiliki keunggulan lokal dan global serta berakhlak mulia Mengutamakan kesiapan siswa untuk memasuki dunia kerja sertamengembangkankeprofesionalan Menyiapkan tenaga menengah kejuruan yang terampil berakhlak
+                mulia untuk mengisi kebutuhan dunia usaha dan industri Menyiapkan siswa agar menjadi warga negara yang produktif Menyiapkan siswa agar dapat berwirausaha secara mandiri Menyiapkan siswa agar dapat melanjutkan pedidikan ke
+                jenjang yang lebih tinggi
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="mb-10">
+          <h1 className="m-3 pt-5 font-bold italic text-lg md:ml-10">Latest News → </h1>
+          <div className="md:flex  ">
+            <div
+              style={{ backgroundImage: `url(${gbk2})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}
+              className="min-w-[330px]  md:w-1/3 h-[420px]  m-5  rounded-xl relative drop-shadow-xl xl:mb-10 "
+            >
+              <Link to="/News1">
+                <button className="absolute bottom-5 ml-6 px-6 py-3 bg-white/60 hover:bg-white/80 transition duration-300">Lihat Berita</button>
+              </Link>
+            </div>
+            <div className="mb-10 mt-5 flex md:block md:w-full md:mr-5 xl:flex ">
+              <div
+                style={{ backgroundImage: `url(${gbk3})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}
+                className="w-1/2  ml-5 md:w-full md:mr-5 md:ml-0 h-[200px] xl:h-full    bg-slate-400 rounded-xl drop-shadow-xl relative flex"
+              >
+                <Link to="/News1">
+                  <button className="absolute bottom-4 ml-4 px-5 py-2 bg-white/60 hover:bg-white/80 transition duration-300">Lihat Berita</button>
+                </Link>
+              </div>
+              <div
+                style={{ backgroundImage: `url(${gbk2})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}
+                className="w-1/2 mr-5 ml-5 md:w-full md:mr-5 md:ml-0 h-[200px] xl:h-full md:mt-5 xl:mt-0    bg-slate-400 rounded-xl drop-shadow-xl relative flex"
+              >
+                <Link to="/News1">
+                  <button className="absolute bottom-4 ml-4 px-5 py-2 bg-white/60 hover:bg-white/80 transition duration-300">Lihat Berita</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
