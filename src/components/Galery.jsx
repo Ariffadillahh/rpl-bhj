300e2661bfb43f06603dff54eb8dc6b116d53e42
zhettarln <zhettanita31@gmail.com>
2022-07-11 12:38:23 +0700

    Update galery

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -8,11 +8,24 @@ const Galery = () => {
       <div ><img src={bunga} className="mx-auto mt-10" /></div>
       <h1 className="text-center uppercase mt-7 text-lg font-bold ">Gallery rpl smk bhumi husada <span className="block w-[100px] h-1 my-7 rounded-lg bg-[#034867] text-center mx-auto" ></span></h1>
       <div className="flex mx-5 pt-5">
-        <div className="w-full pb-5 bg-[#E9E9E9] mx-10 rounded-lg mb-10 md:flex ">
-          <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-          <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-          <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-          
+        <div className="w-full pb-5 bg-[#E9E9E9] mx-10 rounded-lg mb-10 md:flex  ">
+          <div >
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+          </div>
+          <div >
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+          </div>
+          <div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
+          </div>
+         
+         
         </div>
       </div>
     </div>
