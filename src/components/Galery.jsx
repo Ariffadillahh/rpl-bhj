44ebbc2de69f95769cce1e4903b7065a480c13f4
Ariffadillahh <fadilaharif99@gmail.com>
2022-07-11 19:27:31 +0700

    anjay susah cok

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -1,35 +1,64 @@
-import React from "react";
+import Lightroom from "react-lightbox-gallery";
 import bunga from "../source/bunga.png";
-import okee from "../source/okee.jpeg"
-
-const Galery = () => {
-  return <div>
+export default function App() {
+  var images = [
+    {
+      src: "https://images.unsplash.com/photo-1577279549270-b9e297533cdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+      desc: "Person wearing shoes",
+      sub: "Gift Habeshaw",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1577277625082-36df4915ebeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+      desc: "Blonde woman wearing sunglasses smiling at the camera ",
+      sub: "Dmitriy Frantsev",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1577278689329-1914b6814d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1140&q=80",
+      desc: "Man and woman passing by store",
+      sub: "Harry Cunningham",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1577177205947-4078546be974?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+      desc: "Jaipur , Rajasthan India",
+      sub: "Liam Baldock",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1437326300822-01d8f13c024f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      sub: "Verne Ho",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1549468057-5b7fa1a41d7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
+      desc: "Rann of kutch , India",
+      sub: "Hari Nandakumar",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1521859727374-9b8e0ef8c0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+      sub: "Jyotirmoy Gupta",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1577201404214-b725e3212fc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+      sub: "Tom & Sini",
+    },
+  ];
+  var settings = {
+    columnCount: {
+      default: 3,
+      mobile: 1,
+      tab: 4,
+    },
+    mode: "dark",
+  };
+  return (
     <div>
-      <div ><img src={bunga} className="mx-auto mt-10" /></div>
-      <h1 className="text-center uppercase mt-7 text-lg font-bold ">Gallery rpl smk bhumi husada <span className="block w-[100px] h-1 my-7 rounded-lg bg-[#034867] text-center mx-auto" ></span></h1>
-      <div className="flex mx-5 pt-5">
-        <div className="w-full pb-5 bg-[#E9E9E9] mx-10 rounded-lg mb-10 md:flex  ">
-          <div >
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-          </div>
-          <div >
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-          </div>
-          <div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-            <div><img src={okee} className="w-full  drop-shadow-lg  rounded-lg p-5 " /></div>
-          </div>
-         
-         
-        </div>
+      <div>
+        <img src={bunga} className="mx-auto mt-10" />
+      </div>
+      <h1 className="text-center uppercase mt-7 text-lg font-bold ">
+        Gallery rpl smk bhumi husada <span className="block w-[100px] h-1 my-7 rounded-lg bg-[#034867] text-center mx-auto"></span>
+      </h1>
+      <div className="p-10">
+        <Lightroom images={images} settings={settings} />
       </div>
     </div>
-  </div>;
-};
-
-export default Galery;
+  );
+}
